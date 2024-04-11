61ff9f3a09922f668ada61991123540a555362f2
belplaton <platonbeliakov@gmail.com>
2024-04-11 23:10:02 +0300

    feat: reconstrucion from vector

--- Graphs/ClassLibrary1/Graph.cs
+++ Graphs/ClassLibrary1/Graph.cs
@@ -2,6 +2,57 @@
 {
     public class Graph
     {
+        public static int[,] ReconstructGraph(int[] degreeVector)
+        {
+            if (degreeVector == null)
+            {
+                throw new ArgumentNullException(nameof(degreeVector));
+            }
+            
+            if (degreeVector.Sum() % 2 != 0)
+            {
+                Console.WriteLine("Degree sequence is not graphical. Cannot reconstruct the graph.");
+                return new int[0, 0];
+            }
 
+            var vertices = degreeVector.Count();
+            var adjacencyMatrix = new int[vertices, vertices];
+
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = i + 1; j < vertices; j++)
+                {
+                    if (degreeVector[i] > 0 && degreeVector[j] > 0)
+                    {
+                        adjacencyMatrix[i, j] = 1;
+                        adjacencyMatrix[j, i] = 1;
+                        degreeVector[i]--;
+                        degreeVector[j]--;
+                    }
+                }
+            }
+
+            return adjacencyMatrix;
+        }
+
+        public static void PrintGraph(int[,] matrix)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+
+            var vertices = matrix.GetLength(0);
+
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
---
+++ Graphs/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using HyperGraphs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var degreeVector = new int[] { 1, 1, 1, 1 };
+        var reconstructedGraph = Graph.ReconstructGraph(degreeVector);
+
+        Graph.PrintGraph(reconstructedGraph);
+
+        Console.ReadLine();
+    }
+}
