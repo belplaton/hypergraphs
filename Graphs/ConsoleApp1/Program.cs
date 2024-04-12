6a9eaf1d48966f1c050f4c84ee20c29cf400fcfe
belplaton <platonbeliakov@gmail.com>
2024-04-13 00:44:32 +0300

    test

--- Graphs/ConsoleApp1/Program.cs
+++ Graphs/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var degreeVector = new int[] { 1, 1, 1, 1 };
+        var degreeVector = new int[] { 1, 2, 2, 1 };
         var reconstructedGraph = Graph.ReconstructGraph(degreeVector);
 
         Graph.PrintGraph(reconstructedGraph);
